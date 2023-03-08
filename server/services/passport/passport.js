1da063357f793cb8bb13a5aa187b118d5d6d5d8a
santosh 2 <santehero8@gmail.com>
2023-03-08 23:38:24 +0545

    server side changed

--- server/services/passport/passport.js
+++ server/services/passport/passport.js
@@ -5,7 +5,7 @@ const { createGoogleUser } = require('../authService/AuthService');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://vrumies-api.vercel.app/api/passport/google/callback",
+    callbackURL: process.env.CALLBACK_URL,
     passReqToCallback: true
 },
     async function (request, accessToken, refreshToken, profile, done) {
